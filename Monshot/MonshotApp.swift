9351d5881dbce9ed0470e2ba5203bb546d0cbfb7
TarasKarabas <taraskyparenko@gmail.com>
2023-04-23 18:01:21 +1030

    Initial Commit

---
+++ Monshot/MonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonshotApp.swift
+//  Monshot
+//
+//  Created by Taras Kyparenko on 23/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
